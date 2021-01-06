766464ea7e005d96a7199f04324025c6035bf40f
Bartek Kasprzyk <dolarbartek@wp.pl>
2021-01-06 21:12:06 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function myFunction1() {
+    var element = document.getElementById("btn1");
+    element.classList.add("mystyle");
+  }
+function myFunction2() {
+    var element = document.getElementById("btn2");
+    element.classList.add("mystyle");
+  }
+function myFunction3() {
+    var element = document.getElementById("btn3");
+    element.classList.add("mystyle");
+  }
+function myFunction4() {
+    var element = document.getElementById("btn4");
+    element.classList.add("mystyle");
+  }
+function myFunction5() {
+    var element = document.getElementById("btn5");
+   element.classList.add("mystyle");
+  }
